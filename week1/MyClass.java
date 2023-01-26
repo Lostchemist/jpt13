754fd6570b1f1351e6068a40606099810ef997a1
amr <dquijote22@gmail.com>
2023-01-26 21:24:08 +0100

    más modificar

--- week1/MyClass.java
+++ week1/MyClass.java
@@ -1,8 +1,16 @@
+/**
+ * descripción de mi claes
+ */
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.println();
+        String word = "Hello";
 
+        // String methods
+        System.out.println(word + " World!");
+        System.out.println(word.length());
+        System.out.println(word.toUpperCase());
+        System.out.println(word.toLowerCase());
     }
 
 }
