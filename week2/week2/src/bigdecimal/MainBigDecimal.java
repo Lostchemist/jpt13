761ec3dc77e55781f28a929bb0c3ca94dbf2f7ec
amr <dquijote22@gmail.com>
2023-02-04 12:03:16 +0100

    week2 project

---
+++ week2/week2/src/bigdecimal/MainBigDecimal.java
@@ -0,0 +1,30 @@
+package bigdecimal;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class MainBigDecimal {
+
+
+    public static void main(String[] args) {
+        BigDecimal eqResult = calculateEquation(new BigDecimal(10)).setScale(2, RoundingMode.CEILING);
+        System.out.println(eqResult);
+
+    }
+
+
+    public static BigDecimal calculateEquation(BigDecimal x) {
+        BigDecimal twoPointThree = new BigDecimal(2.3);
+        BigDecimal onePointFive = new BigDecimal(1.5);
+        BigDecimal pointEight = new BigDecimal(0.8);
+
+
+        BigDecimal leftSide = x.multiply(twoPointThree).add(onePointFive);
+        BigDecimal rightSide = x.subtract(pointEight);
+
+        BigDecimal result = leftSide.divide(rightSide);
+
+        return result;
+    }
+}
