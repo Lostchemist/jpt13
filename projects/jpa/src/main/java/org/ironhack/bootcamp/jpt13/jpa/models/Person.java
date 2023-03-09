13299f1ba75284bba7eeb9b0771441eeae0038bb
amr <dquijote22@gmail.com>
2023-03-09 17:51:14 +0100

    ADd new project with more examples

---
+++ projects/jpa/src/main/java/org/ironhack/bootcamp/jpt13/jpa/models/Person.java
@@ -0,0 +1,42 @@
+package org.ironhack.bootcamp.jpt13.jpa.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="person")
+public class Person {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ projects/jpa/src/test/java/org/ironhack/bootcamp/jpt13/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ironhack.bootcamp.jpt13.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
